749d0218c65f507ba123da8499491e90534472b2
Zaid <itzzaid@pm.me>
2022-10-10 11:32:13 +0530

    Update cloner.py

--- Zaid/plugins/cloner.py
+++ Zaid/plugins/cloner.py
@@ -2,6 +2,8 @@ from Zaid import HANDLER, Zaid
 from telethon import TelegramClient, events
 from config import API_ID, API_HASH
 import sys
+import telethon
+
 
 @Zaid.on(events.NewMessage(pattern="^[?!/]clone"))
 async def clone(msg):
@@ -12,8 +14,6 @@ async def clone(msg):
         await text.edit("Booting Your Client")
         client = TelegramClient(":memory:", api_id=API_ID, api_hash=API_HASH)
         await client.start(bot_token=phone)
-        sys.modules["Zaid.plugins." + plugin_name] = load
-        await text.edit("🔥 Booted Plugin⚡ " + plugin_name)
         user = await client.get_me()
         userid = telethon.utils.get_peer_id(user)
         await msg.reply(f"Your Client Has Been Successfully Started As {userid}! ✅\n\nThanks for Cloning.\nDon't Forget to Join Our @TheUpdatesChannel")
