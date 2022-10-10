e80e905a228ec887c27766d3f8a9b4ec9a7b3044
Zaid <itsunknown122@gmail.com>
2022-10-10 10:13:58 +0530

    Create start.py

---
+++ Zaid/plugins/start.py
@@ -0,0 +1,42 @@
+from Zaid import HANDLER
+from telethon import events, Button
+
+START_IMG = "https://telegra.ph/file/4cee76750b35007053c0d.jpg"
+PM_START_TEXT = """
+ʜᴇʏᴀ! {}
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ **ɪ'ᴍ ᴀ ꜱɪᴍᴘʟᴇ ᴛᴇʟᴇɢʀᴀᴍ ʙᴏᴛ**.
+➖➖➖➖➖➖➖➖➖➖➖➖➖➖
+✘ **ᴄʟɪᴄᴋ ᴏɴ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ 🔘 ꜰᴏʀ ᴍᴏʀᴇ ɪɴꜰᴏʀᴍᴀᴛɪᴏɴ ℹ️**.
+"""
+
+@events.register(events.NewMessage(pattern="^[?!/]start$"))
+async def start(event):
+    if event.is_private:
+       await event.client.send_file(event.chat_id,
+             START_IMG,
+             caption=PM_START_TEXT.format(event.sender.first_name), 
+             buttons=[
+        [Button.url("👨‍💻 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", "https://github.com/ITZ-ZAID/Telethon-Music")],
+        [Button.url("🗣️ ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("📣 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help")]])
+       return
+
+    if event.is_group:
+       await event.reply("**ʜᴇʏ! ɪ'ᴍ ꜱᴛɪʟʟ ᴀʟɪᴠᴇ ✅**")
+       return
+
+
+
+@events.register(events.callbackquery.CallbackQuery(data="start"))
+async def stac(event):
+    if event.is_private:
+       await event.edit(PM_START_TEXT.format(event.sender.first_name), buttons=[
+        [Button.url("👨‍💻 ꜱᴏᴜʀᴄᴇ ᴄᴏᴅᴇ", "https://github.com/ITZ-ZAID/Telethon-Music")],
+        [Button.url("🗣️ ꜱᴜᴘᴘᴏʀᴛ", f"https://t.me/{Config.SUPPORT}"), Button.url("📣 ᴜᴘᴅᴀᴛᴇꜱ", f"https://t.me/{Config.CHANNEL}")],
+        [Button.inline("ʜᴇʟᴘ ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅꜱ", data="help")]])
+       return
+
+
+HANDLER.append(stac)
+HANDLER.append(start)
