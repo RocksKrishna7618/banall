d22d29aa34d40ce6f6013526bde150eed107af89
Zaid <itsunknown122@gmail.com>
2022-10-10 09:56:12 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -5,4 +5,4 @@ from config import API_ID, API_HASH, BOT_TOKEN
 
 
 
-Zaid1 = TelegramClient('Cli1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
+Zaid = TelegramClient('Cli1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
