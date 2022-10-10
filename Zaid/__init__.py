8e2f7e43f5ebce65916b51c0966a25556b3ce943
Zaid <itzzaid@pm.me>
2022-10-10 10:48:32 +0530

    Update __init__.py

--- Zaid/__init__.py
+++ Zaid/__init__.py
@@ -1,8 +1,9 @@
 from telethon import TelegramClient
-from config import API_ID, API_HASH, BOT_TOKEN
+from config import API_ID, API_HASH, BOT_TOKEN, SUDO_USER
 
 HANDLER = [] #jugar h vai jayda mat soch
-
+SUDO_USERS = []
+SUDO_USERS.append(SUDO_USER)
 
 
 Zaid = TelegramClient('Cli1', API_ID, API_HASH).start(bot_token=BOT_TOKEN)
