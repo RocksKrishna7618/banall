0a5308a75843b26a5bb591ee60dcfa3d4d62b78c
Zaid <itzzaid@pm.me>
2022-10-10 10:22:04 +0530

    Update __main__.py

--- Zaid/__main__.py
+++ Zaid/__main__.py
@@ -1,5 +1,15 @@
 from Zaid import Zaid
+import glob
+from pathlib import Path
+from Zaid.utils import load_plugins
 
+path = "Zaid/plugins/*.py"
+files = glob.glob(path)
+for name in files:
+    with open(name) as a:
+        patt = Path(a.name)
+        plugin_name = patt.stem
+        load_plugins(plugin_name.replace(".py", ""))
 
 if __name__ == "__main__":
     print("[INFO]: SUCCESSFULLY STARTED BOT!")
